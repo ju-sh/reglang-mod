73010cbfb794c74f2679289d3cdd66e29c4d980c
Julin Shaji <112103001@smail.iitpkd.ac.in>
2024-03-27 15:38:41 +0530

    start making nfa to dfa

--- theories/dfa.v
+++ theories/dfa.v
@@ -7,17 +7,55 @@ From aruvi Require nfa.
 Import state.StateNotations.
 
 Record t {A: Type}: Type := mkDfa {
-  state: state.tDfa;
-  start: dfa⟦state⟧;
-  final: {set dfa⟦state⟧};
-  tf: dfa⟦state⟧ -> A -> dfa⟦state⟧
+  state: state.tNfa;
+  start: {sset nfa⟦state⟧};
+  final: {sset nfa⟦state⟧};
+  tf: {sset nfa⟦state⟧} -> A -> {sset nfa⟦state⟧}
 }.
 Arguments t: clear implicits.
 
-(* Definition of_nfa {A: Type} (n: nfa.t A): t A := {| *)
-(*   state := state.pset (nfa.state n); *)
-(*   start := (nfa.start n) A; *)
-(*   final := [set X | X :&: nfa_fin A != set0]; *)
-(*   tf X a := [set q | [exists (p | p \in X), nfa_trans p a q]] *) 
-(* |}. *)
+Definition of_nfa {A: Type} (n: nfa.t A): t A. refine {|
+  state := nfa.state n;
+  start := _; (* [set (nfa.start n)]; *)
+  (* start := [set [set x] | x in nfa.start n]; (1* [set (nfa.start n)]; *1) *)
+  final := _;
+  tf src ch := _
+|}.
+- Check [set  
+- Check [set: dfa⟦state.pset (nfa.state n)⟧].
+  Check [set x | x in [set: dfa⟦state.pset (nfa.state n)⟧]].
+  Check [set x | x in [set: dfa⟦state.pset (nfa.state n)⟧]].
+  Check [set x | x in [set: bool]].
+  Check [set x in [set: bool] | true].
+  Check [set x in [set: dfa⟦state.pset (nfa.state n)⟧] | true].
+
+  Check [set x in [set: _] | true].
+  Check [set x in [set: {set {set nfa⟦nfa.state n⟧}}] | true].
+  Search {set _} bool.
+  Search (set_of _ -> set_of _ -> bool).
+  Check [set x in [set: {set {set nfa⟦nfa.state n⟧}}] | x :&: _ = false].
+
+  Check [set x in [set: dfa⟦state.pset (nfa.state n)⟧] | x :&: _].
+(*
+[set p | p ∩ n.final != ∅
+     & p in pset] *)
+
+Check [set true; false].
+Check [set true] == set0.
+(* [set true; false] : {set Datatypes_bool__canonical__fintype_Finite} *)
+- Check [set [set x] | x in nfa.final n]. 
+Check [set: bool].
+Check [set: bool].
+(* [set: bool] : {set Datatypes_bool__canonical__fintype_Finite} *)
+
+
+- Check nfa.state n.
+  exact: [set (nfa.start n)].
+  Check {set nfa⟦nfa.state n⟧}.
+  Check [set x | x \in {set nfa⟦nfa.state n⟧}].
+  Check [set x | set0 \notin (x :&: (nfa.start n)) & x \in {set nfa⟦nfa.state n⟧}].
+  Check [set x | set0 \notin (x :&: (nfa.start n)) & x \in {set nfa⟦nfa.state n⟧}].
+  Check [set x | x \in {set nfa⟦state⟧}].
+  + exact: 
+  Check [set X | X :&: nfa.final n != set0].
 
--- theories/state.v
+++ theories/state.v
@@ -23,12 +23,12 @@ Fixpoint tNfaDenote (s: tNfa): finType :=
   end)%type
 where "'nfa⟦' s '⟧'" := (tNfaDenote s).
 
-Fixpoint tDfaDenote (s: tDfa): Type :=
-  match s with
+Fixpoint tDfaDenote (s: tDfa): finType :=
+  (match s with
   | DOne => unit
   | DPlus a b => dfa⟦a⟧ + dfa⟦b⟧
   | DMult a b => dfa⟦a⟧ * dfa⟦b⟧
-  end
+  end)%type
 where "'dfa⟦' s '⟧'" := (tDfaDenote s).
 
 Fixpoint pset (s: tNfa): tDfa :=
@@ -38,10 +38,63 @@ Fixpoint pset (s: tNfa): tDfa :=
   | NPlus a b => DMult (pset a) (pset b)
   end.
 
+(* Fixpoint tNfaEnum (s: tDfa): seq (dfa⟦s⟧). *)
+(*   refine ( *)
+(*   match s with *)
+(*   | DOne => [:: tt] *)
+(*   | DPlus a b => _ *)
+(*   | DMult a b => _ *)
+(*   end) => /=. *)
+
+
+(* Fixpoint union {n: tNfa}: {set nfa⟦n⟧} -> {set dfa⟦pset n⟧}. *)
+(* refine ( *)
+(*   match n with *)
+(*   | NZero => fun s => set0 *)
+(*   | NOne => fun s => _ *)
+(*   | NPlus a b => fun s => _ *)
+(*   end) => /=; rewrite /= in s. *)
+(*  - shelve. *)
+(*  - *) 
+
+(* Check [set x | x in s]. *)
+(* Check [set (fun x => x) x | x in s]. *)
+(*  - refine ([set _ | x in s]). *)
+(*    Search (set_of _ -> bool). *)
+(*    Search set0 bool. *)
+(*    Compute set0 \in set0. *)
+
+(*    refine ( *)
+(*      match x with *)
+(*      | inl l => inl x' *)
+(*      | inr r => inr x' *)
+
+
+(* ref [set *)
+(*   (match x with *)
+(*   | inl x' => inl x' *)
+(*   | inr x' => inr x' *)
+(*   end) *)
+(*    | x in s]. *)
+
+(* Check [set *)
+(*   (match x with *)
+(*   | inl x' => x *)
+(*   | inr x' => x *)
+(*   end) *)
+(*    | x in s]. *)
+(* Search ({set _} -> seq _). *)
+(* Search (set_of _ -> seq _). *)
+(* Search (set_of _) seq. *)
+(* Search (set_of _) bool. *)
+(* Check [set *) 
+
 Module StateNotations.
   Declare Scope state_scope.
   Delimit Scope state_scope with state.
 
   Notation "'dfa⟦' s '⟧'" := (tDfaDenote s) (at level 20).
   Notation "'nfa⟦' s '⟧'" := (tNfaDenote s) (at level 20).
+
+  Notation "{ 'sset' T }" := {set {set T}} (only parsing): type_scope.
 End StateNotations.
